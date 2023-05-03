3ff2ed81fd9f9186e6556e99026e63aa17539b99
qiunian <qiunian@bytedance.com>
2023-05-03 20:55:55 +0800

    test: finished

--- index.ts
+++ index.ts
@@ -3,8 +3,9 @@ function multiple(a: number, b: number): number {
 }
 
 const res = multiple(2, 5);
-console.log(1234);
 function sum(a:number, b: number): number {
   return a + b;
 }
 const sumRes = sum(1, 2);
+
+console.log('test finished');
